0249eb619775f36abc5eb400bf0c47cbcbb2b01b
thradams <thiago@elipse.com.br>
2023-08-02 10:01:22 -0300

    fixing dpi awareness

--- installer/make_installer_debug.bat
+++ installer/make_installer_debug.bat
@@ -11,7 +11,8 @@ makezip.exe
 
 
 rc /D "_UNICODE" /D "UNICODE" /l 0x0409 /nologo /fo"installer.res"  installer.rc 
-cl  /Zi /Od /MDd /RTC1 /D_CRT_SECURE_NO_WARNINGS /DWIN32 /D_DEBUG /D_WINDOWS /D_UNICODE /DUNICODE installer.res  installer.c script.c kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib /link /MANIFEST:EMBED /MANIFESTUAC:"level='requireAdministrator' uiAccess='false'"
+cl  /Zi /Od /MDd /RTC1 /D_CRT_SECURE_NO_WARNINGS /DWIN32 /D_DEBUG /D_WINDOWS /D_UNICODE /DUNICODE installer.res  installer.c script.c kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib /link ^
+/MANIFEST:EMBED /MANIFESTINPUT:"installer.exe.manifest" /MANIFESTUAC:"level='requireAdministrator' uiAccess='false'"
 
 REM to debug the installer use
 REM devenv /DebugExe installer.exe
